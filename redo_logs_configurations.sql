50c46c0f49d0ee3e6e0fc7ac20c5d25c0ae631ed
lucian-12 <46110987+lucian-12@users.noreply.github.com>
2021-05-12 13:29:06 +0300

    Update redo_logs_configurations.sql

--- redo_logs_configurations.sql
+++ redo_logs_configurations.sql
@@ -1,3 +1,4 @@
+# Redo Logs related configurations
 SHOW GLOBAL variables like 'innodb_log_buffer_size';
 SHOW GLOBAL variables like 'innodb_log_file_size';
 SHOW GLOBAL variables like 'innodb_log_files_in_group';
